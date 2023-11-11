e4079733659460a925596392d2da1b57b0ff97fb
ThreadGitHub <ThreadEmail@163.com>
2023-11-11 17:48:41 +0800

    update No765
    Signed-off-by: ThreadGitHub <ThreadEmail@163.com>

--- src/main/java/org/leetcode/no_765/No_765_MinSwapsCouples.java
+++ src/main/java/org/leetcode/no_765/No_765_MinSwapsCouples.java
@@ -16,10 +16,13 @@ public class No_765_MinSwapsCouples {
     public static void main(String[] args) {
         System.out.println(0 ^ 1);
         System.out.println(1 ^ 1);
+
         System.out.println(2 ^ 1);
         System.out.println(3 ^ 1);
+
         System.out.println(4 ^ 1);
         System.out.println(5 ^ 1);
+
         System.out.println(6 ^ 1);
         System.out.println(7 ^ 1);
     }
